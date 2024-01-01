3cf46f2761506124fdcea6abb408c53b48a573a1
Pranav Nair <pranav.nair@sculptsoft.com>
2024-01-01 14:08:35 +0530

    prompt change

--- apis/hi.py
+++ apis/hi.py
@@ -1,15 +1,16 @@
 from flask_restful import Resource, request
 from flask import make_response
 import langchain
+import markdown
 
-
+import re
 from utils.error_handler import error_handler
 from utils.logger import get_logger
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 import chromadb
 from langchain.llms import OpenAI
-from langchain.memory import ConversationBufferMemory, RedisChatMessageHistory
+from langchain.memory import ConversationBufferMemory, RedisChatMessageHistory, FileChatMessageHistory
 from langchain import LLMChain
 from langchain.prompts import MessagesPlaceholder, HumanMessagePromptTemplate, ChatPromptTemplate, SystemMessagePromptTemplate
 
@@ -48,22 +49,29 @@ class RetrievalView(Resource):
             return_messages=True,
             llm=llm,
         )
-
+        msg = '''
+        When responding, imagine you are a virtual assistant representing Sculptsoft, a specialized app development company. 
+        Provide answers exclusively based on the given context. 
+        In accordance with the provided {contexts}, please reply to the following {query} as if you were this virtual assistant. 
+        Ensure that your response is solely derived from the provided contexts, refraining from generating answers independently. 
+        If no answer is found, kindly state 'I'm sorry, but I'm unable to provide an answer to that question at the moment. It could be a topic outside my current knowledge or capabilities. If you have another question or need assistance with something else, feel free to ask, and I'll do my best to help!' 
+        Strictly adhere to the given contexts and give answers solely from the provided context.
+        '''
         system_history_msg = '''
-        The following is a friendly conversation between a Human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says "I'm sorry, but I'm unable to provide an answer to that question at the moment.". Don't give answer as AI.
+        The following is a friendly conversation between a Human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says "I'm sorry, but I'm unable to provide an answer to that question at the moment.".
         '''
 
         system_query_msg = '''
         Context: {contexts}
-        When responding, imagine you are a virtual assistant representing Sculptsoft. Provide answers to the below query exclusively based on the above context. Ensure that your response is solely derived from the provided contexts, refraining from generating answers independently.
-        If you don't have answer to the query, kindly state "I'm sorry, but I'm unable to provide an answer to that question at the moment."
+        When responding, imagine you are a virtual assistant representing a specialized app development company. Provide answers exclusively based on the given context. Ensure that your response is solely derived from the provided contexts, refraining from generating answers independently.
+        If you don't have answer to the query, kindly state "I'm sorry, but I'm unable to provide an answer to that question at the moment.". Strictly adhere to the given contexts and give answers solely from the provided context.
         '''
 
         human_query_msg = '''
         Query: {query}
         '''
         system_post_msg = '''
-        Answer text should not contain "AI:" or "System:".
+        Answer text should not contain "AI:" or "System:" and should be more descriptive.
         '''
         # chain = load_qa_chain(OpenAI(), chain_type="stuff")
         prompt = ChatPromptTemplate(
@@ -86,9 +94,102 @@ class RetrievalView(Resource):
         langchain.debug = True
         v = chain({"query": query})
         answer = v.get("text", None)
+        # if (answer := v.get("text", None)) is not None:
+        #     link_pattern = re.compile(r'\[(.*?)\]\((.*?)\)')
+        #     results = link_pattern.findall(answer)
+        #     for result in results:
+        #         link_text, url = result
+        #         markdown_link = f'[link]({url})'
+        #         answer = answer.replace(f'[{link_text}]({url})', markdown_link)
+
         logger = get_logger(session_id=session_id)
         logger.info(str({"query": query, "text": answer, "contexts": contexts}))
         return make_response({"status": True, "detail": answer}, 200)
+
+#
+# class RetrievalView(Resource):
+#
+#     @error_handler
+#     def post(self):
+#         data = request.get_json()
+#         if (query := data.get('query', None)) is None:
+#             return make_response({"status": False, "detail": "query is required"}, 400)
+#         if (session_id := data.get('session_id', None)) is None:
+#             return make_response({"status": False, "detail": "session_id is required"}, 400)
+#
+#         chroma_client = chromadb.HttpClient(host="43.205.120.186", port=8000)
+#         llm = OpenAI(model='gpt-3.5-turbo-instruct', temperature=0.2)
+#         embeddings = OpenAIEmbeddings()
+#         db4 = Chroma(
+#             client=chroma_client,
+#             collection_name="ss_chatbot",
+#             embedding_function=embeddings,
+#         )
+#         # query = "What speciality does sculptsoft provide in mobile app development"
+#         results = db4.similarity_search_with_score(query, k=5)
+#         contexts = ''
+#         for res in results:
+#             contexts += res[0].page_content
+#
+#         loaded_chat_memory = ConversationBufferMemory(
+#             chat_memory=FileChatMessageHistory(f"sessions/chat_history_{str(session_id)}.json"),
+#             # chat_memory=RedisChatMessageHistory(
+#             #     session_id=f"chat_history_{str(session_id)}",
+#             #     url="redis://redis_service:6379/0",
+#             # ),
+#             memory_key="chat_history",
+#             return_messages=True,
+#             llm=llm,
+#         )
+#
+#         system_history_msg = '''
+#         The following is a friendly conversation between a Human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says "I'm sorry, but I'm unable to provide an answer to that question at the moment.".
+#         '''
+#
+#         system_query_msg = '''
+#         Context: {contexts}
+#         When responding, imagine you are a virtual assistant representing Sculptsoft. Provide answers to the below query exclusively based on the above context. Ensure that your response is solely derived from the provided contexts, refraining from generating answers independently.
+#         If you don't have answer to the query, kindly state "I'm sorry, but I'm unable to provide an answer to that question at the moment."
+#         '''
+#
+#         human_query_msg = '''
+#         Query: {query}
+#         '''
+#         system_post_msg = '''
+#         Answer text should not contain "AI:" or "System:".
+#         '''
+#         # chain = load_qa_chain(OpenAI(), chain_type="stuff")
+#         prompt = ChatPromptTemplate(
+#             input_variables=['query'],
+#             messages=[
+#                 SystemMessagePromptTemplate.from_template(system_history_msg),
+#                 MessagesPlaceholder(variable_name="chat_history"),
+#                 SystemMessagePromptTemplate.from_template(system_query_msg, partial_variables={"contexts": contexts}),
+#                 HumanMessagePromptTemplate.from_template(human_query_msg),
+#                 SystemMessagePromptTemplate.from_template(system_post_msg),
+#             ]
+#         )
+#
+#         chain = LLMChain(
+#             llm=llm,
+#             prompt=prompt,
+#             memory=loaded_chat_memory,
+#             verbose=True
+#         )
+#         langchain.debug = True
+#         v = chain({"query": query})
+#         answer = v.get("text", None)
+#         # if (answer := v.get("text", None)) is not None:
+#         #     link_pattern = re.compile(r'\[(.*?)\]\((.*?)\)')
+#         #     results = link_pattern.findall(answer)
+#         #     for result in results:
+#         #         link_text, url = result
+#         #         markdown_link = f'[link]({url})'
+#         #         answer = answer.replace(f'[{link_text}]({url})', markdown_link)
+#
+#         logger = get_logger(session_id=session_id)
+#         logger.info(str({"query": query, "text": answer, "contexts": contexts}))
+#         return make_response({"status": True, "detail": answer}, 200)
 
 
 class TimeoutView(Resource):
